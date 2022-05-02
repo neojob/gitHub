41d71c4befb412cb379b8d16b73167e5eaad0fb3
Qerob <kerob.job@gmail.com>
2022-05-02 22:24:21 +0400

    conflict

--- app/Http/Controllers/Controller3.php
+++ app/Http/Controllers/Controller3.php
@@ -10,4 +10,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller3 extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function bbbb()
+    {
+        
+    }
 }
